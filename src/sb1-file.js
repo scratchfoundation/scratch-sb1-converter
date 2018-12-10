mzgoddard/scratch-sb1-converter-1

decode and preview individual fields in .sb files

src/sb1-file.js
@@ -1,0 +1,51 @@
+import {ByteStream} from './coders/byte-stream';
+
+import {ByteTakeIterator} from './squeak/byte-take-iterator';
+import {FieldIterator} from './squeak/field-iterator';
+
+import {SB1BlockHeader, SB1Signature} from './sb1-file-blocks';
+
+class SB1File {
+    constructor (buffer) {
+        this.buffer = buffer;
+        this.stream = new ByteStream(buffer);
+
+        this.signature = this.stream.readStruct(SB1Signature);
+        this.signature.validate();
+
+        this.infoBlockHeader = this.stream.readStruct(SB1BlockHeader);
+        this.infoBlockHeader.validate();
+
+        this.stream.position += this.signature.infoByteLength - SB1BlockHeader.size;
+
+        this.dataBlockHeader = this.stream.readStruct(SB1BlockHeader);
+        this.dataBlockHeader.validate();
+    }
+
+    view () {
+        return {
+            signature: this.signature,
+            infoBlockHeader: this.infoBlockHeader,
+            dataBlockHeader: this.dataBlockHeader,
+            toString () {
+                return 'SB1File';
+            }
+        };
+    }
+
+    infoRaw () {
+        return new ByteTakeIterator(
+            new FieldIterator(this.buffer, this.infoBlockHeader.offset + SB1BlockHeader.size),
+            this.signature.infoByteLength + SB1Signature.size
+        );
+    }
+
+    dataRaw () {
+        return new ByteTakeIterator(
+            new FieldIterator(this.buffer, this.dataBlockHeader.offset + SB1BlockHeader.size),
+            this.stream.uint8.length
+        );
+    }
+}
+
+export {SB1File};

scratchfoundation/scratch-sb1-converter

src/sb1-file.js
@@ -1,0 +1,51 @@
+import {ByteStream} from './coders/byte-stream';
+
+import {ByteTakeIterator} from './squeak/byte-take-iterator';
+import {FieldIterator} from './squeak/field-iterator';
+
+import {SB1Header, SB1Signature} from './sb1-file-packets';
+
+class SB1File {
+    constructor (buffer) {
+        this.buffer = buffer;
+        this.stream = new ByteStream(buffer);
+
+        this.signature = this.stream.readStruct(SB1Signature);
+        this.signature.validate();
+
+        this.infoHeader = this.stream.readStruct(SB1Header);
+        this.infoHeader.validate();
+
+        this.stream.position += this.signature.infoByteLength - SB1Header.size;
+
+        this.dataHeader = this.stream.readStruct(SB1Header);
+        this.dataHeader.validate();
+    }
+
+    view () {
+        return {
+            signature: this.signature,
+            infoHeader: this.infoHeader,
+            dataHeader: this.dataHeader,
+            toString () {
+                return 'SB1File';
+            }
+        };
+    }
+
+    infoRaw () {
+        return new ByteTakeIterator(
+            new FieldIterator(this.buffer, this.infoHeader.offset + SB1Header.size),
+            this.signature.infoByteLength + SB1Signature.size
+        );
+    }
+
+    dataRaw () {
+        return new ByteTakeIterator(
+            new FieldIterator(this.buffer, this.dataHeader.offset + SB1Header.size),
+            this.stream.uint8.length
+        );
+    }
+}
+
+export {SB1File};